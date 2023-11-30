5839d1913ac21d0c8d9301bd45f2fd7ab06bf34e
Lucas <malara.career.d9mwd@slmail.me>
2023-11-30 15:44:14 +0100

    Init project.

---
+++ src/test/java/com/example/soap/example/SoapDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.soap.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
